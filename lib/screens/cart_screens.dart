02d63374109443e546578b94d20fc5449f11cb84
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-21 00:11:07 +0530

    Items can be deleted

--- lib/screens/cart_screens.dart
+++ lib/screens/cart_screens.dart
@@ -52,6 +52,7 @@ class CartScreen extends StatelessWidget {
               child: ListView.builder(
             itemBuilder: (ctx, index) => CartItem(
               id: cart.items.values.toList()[index].id,
+              productid: cart.items.keys.toList()[index],
               title: cart.items.values.toList()[index].title,
               quantity: cart.items.values.toList()[index].quantity,
               price: cart.items.values.toList()[index].price,
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,30 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
+
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productid;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem({this.id, this.price, this.quantity, this.title});
+  CartItem({this.id, this.productid, this.price, this.quantity, this.title});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-                padding: EdgeInsets.all(5),
-                child: FittedBox(child: Text('\$$price'))),
-          ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
+    return Dismissible(
+      key: ValueKey(id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productid);
+      },
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
         ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: FittedBox(child: Text('\$$price'))),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
+          ),
+        ),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
     );
   }
 }
