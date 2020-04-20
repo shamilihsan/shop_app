ca0f153b34473c76aab25281a91f79a88e0a4f92
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-21 00:58:38 +0530

    Fixed bug

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/widgets/app_drawer.dart';
 
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
-    print(orderData.orders);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
       body: ListView.builder(
-        itemBuilder: (ctx, index) => OrderItem(orderData.orders[index]),
         itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
       ),
     );
   }
