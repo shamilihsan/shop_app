4bc289187749f8b6281c5f091064b7fe22085b21
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-21 00:36:45 +0530

    Actual adds an order

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
           value: Cart(),
         ),
         ChangeNotifierProvider.value(
-          value: Orders(),
+          value: Orders (),
         ),
       ],
       child: MaterialApp(
