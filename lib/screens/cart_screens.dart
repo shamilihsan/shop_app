c73c55365d7f245a4c008058ce0e7f56e9302a92
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-20 23:59:04 +0530

    Items can be added to the cart

--- lib/screens/cart_screens.dart
+++ lib/screens/cart_screens.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -46,6 +47,17 @@ class CartScreen extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (ctx, index) => CartItem(
+              id: cart.items.values.toList()[index].id,
+              title: cart.items.values.toList()[index].title,
+              quantity: cart.items.values.toList()[index].quantity,
+              price: cart.items.values.toList()[index].price,
+            ),
+            itemCount: cart.itemCount,
+          ))
         ],
       ),
     );
