cf5a50bae2d6cddb5f6c2390e350f457cf3035cf
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-26 16:35:08 +0530

    Add order

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -25,17 +25,37 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+
+    final url = 'https://flutter-update.firebaseio.com/orders.json';
+
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
         products: cartProducts,
       ),
     );
     notifyListeners();
+  }
   }
 
   Future<void> fetchAndSetOrders() async {
